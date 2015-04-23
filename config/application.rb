c64adf1bd354b817bfa61c7ca0d3e7bc88ac2217
Yann Lugrin <yann.lugrin@sans-savoir.net>
2015-04-23 22:30:23 +0200

    configure rspec

--- config/application.rb
+++ config/application.rb
@@ -21,6 +21,22 @@ module StopWatch
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
 
+    config.generators do |g|
+      g.orm                 :active_record
+      g.template_engine     nil
+
+      g.assets              false
+      g.stylesheets         false
+      g.stylesheet_engine   nil
+      g.javascripts         false
+      g.helper              false
+
+      g.test_framework      :rspec, fixtures: true
+      g.view_specs          false
+      g.request_specs       false
+      g.fixture_replacement :factory_girl, dir: 'spec/factories'
+    end
+
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
     # config.time_zone = 'Central Time (US & Canada)'
